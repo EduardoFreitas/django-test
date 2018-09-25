c8df9c1fe0b835ea305c3b4ed6a969fac51c112c
EduardoFreitas <eduardo.freitas.cp@gmail.com>
2018-09-24 23:36:56 -0300

    add POST PUT PATCH and DELETE functions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import serializers
+
+
+class HelloApiView(APIView):
+    """ Test API View """
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """ Return a list of API View features """
+
+        an_apiviem = [
+            'Uses HTTP method',
+            'Its similar to a traditional Django',
+            'Its mapped manually'
+        ]
+
+        return Response({'message': 'Hello', 'an_api': an_apiviem})
+
+    def post(self, request):
+        """ Create hello message with name """
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # pk is primary key
+    def put(self, request, pk=None):
+        """ Update objects"""
+
+        return Response({'method': 'put'})
+
+    # pk is primary key
+    def patch(self, request, pk=None):
+        """ Patch - Partial Update objects"""
+
+        return Response({'method': 'patch'})
+
+    # pk is primary key
+    def delete(self, request, pk=None):
+        """ Delete objects"""
+
+        return Response({'method': 'delete'})
